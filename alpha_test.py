3c62afb813f393eb6ff07dbac912faf8ec8887c8
JRScelza <jerome.scelza@mssm.edu>
2018-01-15 17:21:31 -0500

    model

--- alpha_test.py
+++ alpha_test.py
@@ -13,17 +13,6 @@ import os
 from matplotlib import pyplot as plt
 from roi import region_loc
 
-#Pulling in all of the images.  Image 179 of each set is the final picture taken (gives the most amount of time to make sure the test strip is stable)
-
-f_1 = os.path.expanduser("~/Desktop/ImagesOova/variant_1_1/image179.jpg")
-f_2 = os.path.expanduser("~/Desktop/ImagesOova/variant_2_1/image179.jpg")
-f_3 = os.path.expanduser("~/Desktop/ImagesOova/variant_3_1/image179.jpg")
-f_4 = os.path.expanduser("~/Desktop/ImagesOova/variant_4_1/image179.jpg")
-f_5 = os.path.expanduser("~/Desktop/ImagesOova/variant_5_1/image179.jpg")
-f_6 = os.path.expanduser("~/Desktop/ImagesOova/variant_6_1/image179.jpg")
-f_blank = os.path.expanduser("~/Desktop/ImagesOova/variant_blank_1/image179.jpg")
-
-
 #You can pull in a random image from variant_1 to test the code.  Through the time series the image line intensity of variant_1 gets darker and darker.  You can grab it at
 #different points to "pretend" you have more test strips.
 
@@ -31,34 +20,19 @@ image_number = 100
 
 f_test = os.path.expanduser("~/Desktop/ImagesOova/variant_4_1/image%s.jpg" % image_number)
 
-
 bounds , spike_pt  = region_loc(f_test)
-
-variant_1 = cv2.imread(f_1)
-variant_2 = cv2.imread(f_2)
-variant_3 = cv2.imread(f_3)
-variant_4 = cv2.imread(f_4)
-variant_5 = cv2.imread(f_5)
-variant_6 = cv2.imread(f_6)
-variant_blank = cv2.imread(f_blank)
 
 variant_test = cv2.imread(f_test)
 
 min_thresh = 126
 
-ret,thresh1 = cv2.threshold(variant_1,min_thresh,255,cv2.THRESH_BINARY)
-ret,thresh2 = cv2.threshold(variant_2,min_thresh,255,cv2.THRESH_BINARY)
-ret,thresh3 = cv2.threshold(variant_3,min_thresh,255,cv2.THRESH_BINARY)
-ret,thresh4 = cv2.threshold(variant_4,min_thresh,255,cv2.THRESH_BINARY)
-ret,thresh5 = cv2.threshold(variant_5,min_thresh,255,cv2.THRESH_BINARY)
-ret,thresh6 = cv2.threshold(variant_6,min_thresh,255,cv2.THRESH_BINARY)
-ret,threshblank = cv2.threshold(variant_blank,min_thresh,255,cv2.THRESH_BINARY)
-
-
 ret,threshtest = cv2.threshold(variant_test,min_thresh,255,cv2.THRESH_BINARY)
 
 
-rows,cols = variant_1.shape[:2]
+#rows,cols = variant_1.shape[:2]
+rows,cols = variant_test.shape[:2]
+
+
 
 #cv2.rectangle(im,(spike_ind + 70 , B_C - 100),(spike_ind + 1350, B_C + 100),(0,255,0),10)
 
@@ -67,9 +41,8 @@ y = bounds - 100
 w = 1350
 h = 200
 
-mask = np.zeros(variant_1.shape[:2],np.uint8)
+mask = np.zeros(variant_test.shape[:2],np.uint8)
 mask[y:y+h,x:x+w] = 255
-
 
 
 #halfhsv = basehsv[rows/2:rows-1,cols/2:cols-1].copy()  # Take lower half of the base image for testing
@@ -83,18 +56,12 @@ ranges=None
 
 #histbase = cv2.calcHist(basehsv,[0],mask,[0,256],ranges)
 
-variant_1_HIST = cv2.calcHist([thresh1],[2],mask,[256],[0,256])
-variant_2_HIST = cv2.calcHist([thresh2],[2],mask,[256],[0,256])
-variant_3_HIST = cv2.calcHist([thresh3],[2],mask,[256],[0,256])
-variant_4_HIST = cv2.calcHist([thresh4],[2],mask,[256],[0,256])
-variant_5_HIST = cv2.calcHist([thresh5],[2],mask,[256],[0,256])
-variant_6_HIST = cv2.calcHist([thresh6],[2],mask,[256],[0,256])
-variant_blank_HIST = cv2.calcHist([threshblank],[2],mask,[256],[0,256])
+
 
 variant_test_HIST = cv2.calcHist([threshtest],[2],mask,[256],[0,256])
 
 
-res = cv2.bitwise_and(variant_5,variant_5,mask = mask)
+#res = cv2.bitwise_and(variant_5,variant_5,mask = mask)
 
 
 #Creating a list of values that represents the dummy strip line intensity (0, 16.67, 33, etc...)
@@ -112,51 +79,11 @@ for i in range(0,6):
 
 j = 4 # this is the compare function number i am using, you can selcet 0=Correlation, 1=Chi-Square, 2=Intersection, 3=Bhattacharyya, 4=Hellinger
 #for j in range(0,4):    
-Control_Comp = cv2.compareHist(variant_1_HIST,variant_1_HIST,j)
-Control_variant2 = cv2.compareHist(variant_1_HIST,variant_2_HIST,j)
-Control_variant3 = cv2.compareHist(variant_1_HIST,variant_3_HIST,j)
-Control_variant4 = cv2.compareHist(variant_1_HIST,variant_4_HIST,j)
-Control_variant5 = cv2.compareHist(variant_1_HIST,variant_5_HIST,j)
-Control_variant6 = cv2.compareHist(variant_1_HIST,variant_6_HIST,j)
-Control_variantblank = cv2.compareHist(variant_1_HIST,variant_blank_HIST,j)
 
 Control_varianttest = cv2.compareHist(variant_1_HIST,variant_test_HIST,j)
 
 
-x =[Control_Comp, Control_variant2, Control_variant3, Control_variant5,Control_variant4,Control_variant6]
 
-
-plt.show()
-
-fig, ax = plt.subplots()
-#fit = np.polyfit(x, y, deg=1)
-z = np.polyfit(x, y, 1)
-p = np.poly1d(z)
-plt.scatter(x, y),plt.plot(x, p(x)) 
-fig.show()
-
-
-def polyfit(x, y, degree):
-    results = {}
-
-    coeffs = np.polyfit(x, y, degree)
-     # Polynomial Coefficients
-    results['polynomial'] = coeffs.tolist()
-
-    correlation = np.corrcoef(x, y)[0,1]
-
-     # r
-    results['correlation'] = correlation
-     # r-squared
-    results['determination'] = correlation**2
-
-    return results
-
-correlation = polyfit(x, y, 1)['correlation']
-
-print(p(Control_varianttest))
-#
-print(polyfit(x, y, 1))
 
 
 
